08ca5338542ac7945f7926c3696b9e5c4f95e370
whitekite <1075790909@qq.com>
2025-06-28 01:35:47 +0800

    feat!: 删除 `csstype` 依赖相关方法

--- src/dom/style/index.ts
+++ src/dom/style/index.ts
@@ -1,4 +1,2 @@
 export { classNames } from './classNames'
-export { createStyleObject, cso } from './createStyleObject'
-export type { CSSProperties } from './createStyleObject'
 export { isStyleSupport, canUseDocElement, canUseDom } from './styleChecker'
