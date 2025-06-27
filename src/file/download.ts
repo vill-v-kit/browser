411e465d35759bf88acbdd90e46833d129b4ab21
whitekite <1075790909@qq.com>
2025-06-28 01:59:21 +0800

    feat!: 删除 `downloadByUrl` 内部，对特定浏览器的限制

--- src/file/download.ts
+++ src/file/download.ts
@@ -120,33 +120,21 @@ export function downloadByUrl({
   target?: LinkTargetContext
   fileName?: string
 }): boolean {
-  const isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1
-  const isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1
-
-  if (/(iP)/g.test(navigator.userAgent)) {
-    console.error('Your browser does not support download!')
-    return false
+  const link = document.createElement('a')
+  link.href = url
+  link.target = target
+  if (link.download !== undefined) {
+    link.download = fileName || url.substring(url.lastIndexOf('/') + 1, url.length)
   }
-  if (isChrome || isSafari) {
-    const link = document.createElement('a')
-    link.href = url
-    link.target = target
-
-    if (link.download !== undefined) {
-      link.download = fileName || url.substring(url.lastIndexOf('/') + 1, url.length)
-    }
-
-    if (document.createEvent) {
-      const e = document.createEvent('MouseEvents')
-      e.initEvent('click', true, true)
-      link.dispatchEvent(e)
-      return true
-    }
+  if (document.createEvent) {
+    const e = document.createEvent('MouseEvents')
+    e.initEvent('click', true, true)
+    link.dispatchEvent(e)
+    return true
   }
   if (url.indexOf('?') === -1) {
     url += '?download'
   }
-
   openWindow(url, { target })
   return true
 }
